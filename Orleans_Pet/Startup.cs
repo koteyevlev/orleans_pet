555ea1500c587d6910c8555767bc4f46e35675e3
koteyevlev <sdaf@mail.ru>
2021-05-30 17:21:19 +0300

    add swagger

--- Orleans_Pet/Startup.cs
+++ Orleans_Pet/Startup.cs
@@ -27,11 +27,17 @@ namespace Orleans_Pet
             services.AddControllers();
             services.AddTransient<IHelloWorldGrain, HelloWorldGrain>();
             services.AddTransient<IMyStatelessWorker, MyStatelessWorker>();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
